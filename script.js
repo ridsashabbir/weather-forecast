4ac4a549fa7e7b3e7d92e2e21d802101794bb9f1
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-28 18:39:25 +0500

    created package.json with npm init

--- script.js
+++ script.js
@@ -72,4 +72,5 @@ document.addEventListener("DOMContentLoaded", function () {
 const http = require("http");
 const fs = require("fs");
 
-const homeFile = fs.readFileSync("home.html");
+const homeFile = fs.readFileSync("home.html", "utf-8");
+const server = http.createServer((req, res) => {});
