9b9d73e8e4dc7c1c6cb790e632cfd8329a06eeb4
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-29 16:35:51 +0500

    display temperature dynamically using node js

--- script.js
+++ script.js
@@ -69,26 +69,41 @@
 // });
 
 // main backend with node js
+const requires = new require("requires");
 const http = require("http");
 const fs = require("fs");
 let requests = require("requests");
 
 const homeFile = fs.readFileSync("./home.html", "utf-8");
+
+const replaceVal = (tempVal, orgVal) => {
+  let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
+  temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+  temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+  temperature = temperature.replace("{%location%}", orgVal.name);
+  temperature = temperature.replace("{%country%}", orgVal.sys.country);
+  return temperature;
+};
+
 const server = http.createServer((req, res) => {
   if (req.url == "/") {
     requests(
       "https://api.openweathermap.org/data/2.5/weather?q=Lahore&units=metric&appid=bb97dcf45fb2360d40faab747c1ee7fd"
     )
       .on("data", (chunk) => {
-        const objData = JSON.parse(chunk);
-        const arrData = [objData];
-        console.log(arrData[0].main.temp);
-        // console.log(arrData);
-        // console.log(objData);
+        const objdata = JSON.parse(chunk);
+        const arrData = [objdata];
+        //console.log(arrData[0].main.temp);
+        const realTimeData = arrData
+          .map((val) => replaceVal(homeFile, val))
+          .join("");
+
+        res.write(realTimeData);
+        // console.log(realTimeData);
       })
       .on("end", (err) => {
         if (err) return console.log("connection closed due to errors", err);
-
+        res.end();
         console.log("end");
       });
   } else {
